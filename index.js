cf04a9c910f34adc45b00674371094ea86a58823
毕克有 <596737739@qq.com>
2023-03-20 11:30:25 +0800

    feat: 添加源文件和目标文件的校验

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 import path from "path";
 import chalk from "chalk";
+import fs from 'fs';
 import { createExtractorFromFile } from "node-unrar-js";
 import ora from "ora";
 
 const spiner = ora("解压中");
+const file = process.argv[2] || '';
+const sourceFile = path.join(process.cwd(), file + '.rar');
+const targetFile = path.join(process.cwd(), file);
 
 async function extractRarArchive(file, destination) {
   try {
@@ -25,16 +29,23 @@ async function extractRarArchive(file, destination) {
   }
 }
 
-const file = process.argv[2];
-
-console.log(process.argv);
 if (!file) {
   console.log(chalk.red('请输入文件地址'));
   process.exit();
 }
 
+if (!fs.existsSync(sourceFile)) {
+  console.log(chalk.red('文件不存在，请检查文件'));
+  process.exit();
+}
+
+if (fs.existsSync(targetFile)) {
+  console.log(chalk.red('解压过的文件已经存在'));
+  process.exit();
+}
+
 spiner.start();
-extractRarArchive(path.join(process.cwd(), file + '.rar'), path.join(process.cwd(), file));
+extractRarArchive(sourceFile, targetFile);
 
 // 每个 node 版本都是独立的，全局安装的 npm 包也互相独立。
 // 你可以在安装别的版本的时候使用这个参数 `--reinstall-packages-from=current`，它会把当前 node 版本安装的全局 npm 包一并安装到新版本 node 环境里
